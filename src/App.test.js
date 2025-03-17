5a38b4832ec4a47a31f1eb64f7daa3e6f04a39c9
Shadow181013 <101969915+Shadow181013@users.noreply.github.com>
2025-03-17 15:51:54 +0800

    修正測試檔案的問題

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import ChatMessage from '../chatMessage';
+import ChatMessage from './chatMessage';
 
 describe('ChatMessage Component', () => {
     it('renders a text message correctly for a user', () => {
