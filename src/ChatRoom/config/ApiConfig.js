73ebb7fa3669f36a892658643ac1e7a1ec8fd355
Mowei <mowei930425@gmail.com>
2025-07-18 14:12:54 +0800

    將 ApiConfig 中 baseURL 改為環境變數

--- src/ChatRoom/config/ApiConfig.js
+++ src/ChatRoom/config/ApiConfig.js
@@ -1,5 +1,5 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const apiConfig = axios.create({
-	baseURL: `https://feelink-backend.onrender.com`
+	baseURL: process.env.REACT_APP_API_BASE_URL,
 });
